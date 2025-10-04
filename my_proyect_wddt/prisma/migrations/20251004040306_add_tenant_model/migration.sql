0a747b1bc2561e105e89c5688abe67ba66373178
Whazt <walddt05@gmail.com>
2025-10-03 22:03:46 -0600

    Ya funca

---
+++ my_proyect_wddt/prisma/migrations/20251004040306_add_tenant_model/migration.sql
@@ -0,0 +1,38 @@
+/*
+  Warnings:
+
+  - Made the column `name` on table `Tenant` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `telephone` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Tenant" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Tenant" ("createdAt", "id", "name", "updatedAt") SELECT "createdAt", "id", "name", "updatedAt" FROM "Tenant";
+DROP TABLE "Tenant";
+ALTER TABLE "new_Tenant" RENAME TO "Tenant";
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "telephone" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'USER',
+    "tenantId" INTEGER NOT NULL,
+    CONSTRAINT "User_tenantId_fkey" FOREIGN KEY ("tenantId") REFERENCES "Tenant" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_User" ("createdAt", "email", "id", "name", "password", "role", "telephone", "tenantId", "updatedAt") SELECT "createdAt", "email", "id", "name", "password", "role", "telephone", "tenantId", "updatedAt" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
